4b54bd731721796f90d46ba9216c540b45e20fb9
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2020-02-13 18:52:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,14 @@ import * as React from 'react';
 import './App.css';
 import * as DataSource from './datasource.json';
 
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda,
-  EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
 
 class App extends React.Component {
-  private localData: EventSettingsModel = {
-    // dataSource: [{
-    //   EndTime: new Date(2019, 0, 11, 6, 30),
-    //   StartTime: new Date(2019, 0, 11, 4, 0),
-    //   Subject: 'Testing',
-    //   IsAllDay: true,
-    //   RecurrenceRule: 'FREQ=DAILY;INTERVAL=1;COUNT=10'
-    // }],
-    dataSource: DataSource.scheduleData,
-    fields: {
-      id: "Id",
-      subject: { name: 'Subject' },
-      startTime: { name: 'StartTime' },
-      endTime: { name: 'EndTime' },
-      location: { name: 'Location' }
-    }
-  };
+  private schData: Object = DataSource.scheduleData;
+
   public render() {
     return <ScheduleComponent currentView='Month' selectedDate={new Date(2019, 0, 6)} 
-    eventSettings={ this.localData }> 
+    eventSettings={{dataSource: this.schData}}> 
       <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
     </ScheduleComponent>  
   }
